be722bd597ff8243066c34db4d146eb14a19e5b8
Áhim Juhász <ahimjuhasz@gmail.com>
2026-02-16 22:22:04 +0100

    meta fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,11 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Icon Bulls',
+    title: 'Französische bulldogge welpen',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A website about french bulldogs.' }
+      { hid: 'description', name: 'description', content: 'Eine Website über französische Bulldoggen.' }
     ],
     link: [
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
